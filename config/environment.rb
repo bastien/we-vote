6f046d576799075ec664bba076d1862eb09a9398
Bastien Vaucher <bastien.vaucher@gmail.com>
2010-06-27 23:25:01 +0200

    using gmail_smtp again ...

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,16 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 require Rails.root + 'lib/core_extensions/array'
-
-ActionMailer::Base.smtp_settings = {
-  :tls => "true",
-	:address => "smtp.gmail.com",
-	:port => 587,
-	:authentication => :plain,
-	:domain => ENV['GMAIL_SMTP_USER'],
-	:user_name => ENV['GMAIL_SMTP_USER'],
-	:password => ENV['GMAIL_SMTP_PASSWORD'],
-}
+# 
+# ActionMailer::Base.smtp_settings = {
+#   :tls => "true",
+#   :address => "smtp.gmail.com",
+#   :port => 587,
+#   :authentication => :plain,
+#   :domain => ENV['GMAIL_SMTP_USER'],
+#   :user_name => ENV['GMAIL_SMTP_USER'],
+#   :password => ENV['GMAIL_SMTP_PASSWORD'],
+# }
 
 # Initialize the rails application
 WeVote::Application.initialize!
