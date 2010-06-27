854bbd37635f7df1daef0f6183d1a20a24dfe151
Bastien Vaucher <bastien.vaucher@gmail.com>
2010-06-27 22:59:53 +0200

    using action_mailer_optional_tls

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,15 @@
 require File.expand_path('../application', __FILE__)
 require Rails.root + 'lib/core_extensions/array'
 
+ActionMailer::Base.smtp_settings = {
+  :tls => "true",
+	:address => "smtp.gmail.com",
+	:port => 587,
+	:authentication => :plain,
+	:domain => ENV['GMAIL_SMTP_USER'],
+	:user_name => ENV['GMAIL_SMTP_USER'],
+	:password => ENV['GMAIL_SMTP_PASSWORD'],
+}
+
 # Initialize the rails application
 WeVote::Application.initialize!
